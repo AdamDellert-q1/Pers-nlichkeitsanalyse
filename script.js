21116de6c7da54f2986703e1efcfe51601fd1146
ggchel <103545973+ggchel@users.noreply.github.com>
2023-10-21 17:13:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function start(contents, video){
+    contents.style.display = 'none'
+    video.style.display = 'block'  
+}
+function more(){
+    document.write('😘😍Дева придет няшке чтобы поцеловать ее и😔🤫..Не оранжевый,а оранжево черный!😈🤬')
+}
